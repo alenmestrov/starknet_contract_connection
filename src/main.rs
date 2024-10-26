1a7d5d61e72344bf577ef998f691ae23ec4d31d9
alenmestrov <alenmestrov@gmail.com>
2024-10-26 10:20:32 +0200

    implemented custom Type EncodableString for encoding long strings to Felt

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,123 @@
 use std::str::FromStr;
 
-use cainome::cairo_serde::ByteArray;
-use starknet::accounts::{ExecutionEncoding, SingleOwnerAccount};
+use starknet::accounts::{Account, ExecutionEncoding, SingleOwnerAccount};
 use starknet::core::chain_id;
+use starknet::core::utils::get_selector_from_name;
 use starknet::providers::jsonrpc::HttpTransport;
 use starknet::providers::{JsonRpcClient, Url};
-use starknet::core::types::Felt;
+use starknet::core::types::{BlockId, BlockTag, Call, Felt, FunctionCall};
 use starknet::signers::{LocalWallet, SigningKey};
 use starknet_crypto::poseidon_hash_many;
-// use starknet::core::codec::Encode;
+use starknet::core::codec::Encode;
+use starknet::providers::Provider;
 
 mod types;
-use crate::types::*;
+// use crate::types::*;
 
-// use crate::types::{Signed, Request, RequestKind, ContextRequest, ContextRequestKind, Application};
-use cainome::rs::abigen;
-abigen!(ContextConfig, "contract/abi.json", output_path("src/types.rs")); 
+use crate::types::{Signed, Request, RequestKind, ContextRequest, ContextRequestKind, Application, EncodableString};
+
+// use cainome::rs::abigen;
+// abigen!(ContextConfig, "contract/abi.json", output_path("src/types.rs")); 
 
 #[tokio::main]
 async fn main() {
 
-    let contract_id = "0x6b8f57626b689af993a1aa4a5f6efdc24655028eb9be30220521cec407f046c";
-
-    let alice_key = starknet::signers::SigningKey::from_random();
+    let contract_address = "0x2ce0e808c359df2171ab7807871c548a096d49efe36f6e822ed5523197e9428";
+    let relayer_pk = Felt::from_str("0x3466a2196c72a94edd80c49baaad89c3cd71815038c31e1d3c94337ad97406d").unwrap();
+    let relayer_sk = SigningKey::from_secret_scalar(relayer_pk);
+    
+    let alice_key = SigningKey::from_secret_scalar(Felt::from_str("0x402c8f500dd3e61d7405c239e973437fa3e1a45f2e643c470c163a0197153ee").unwrap());
     let alice_public_key = alice_key.verifying_key();
     let alice_public_key_felt = alice_public_key.scalar();
+    println!("alice_public_key_felt: {:?}", alice_public_key_felt);
+    // let alice_public_key_felt_hex = alice_key.secret_scalar();
 
-    let context_key = starknet::signers::SigningKey::from_random();
+    let context_key = SigningKey::from_secret_scalar(Felt::from_str("0x263c2d10461dcbe87a324e9426b6e001e2ff816ed4aec72a1ee2605dcb501b9").unwrap());
     let context_public_key = context_key.verifying_key();
     let context_public_key_felt = context_public_key.scalar();
+    println!("context_public_key_felt: {:?}", context_public_key_felt);
 
-    let application = Application {
-        id: Felt::from_str("0x1234567890abcdef1234567890abcdef123456789").unwrap(),
-        blob: Felt::from_str("0x1234567890abcdef1234567890abcdef123456789").unwrap(),
-        size: 0,
-        source: ByteArray::from_string("https://calimero.network").unwrap(),
-        metadata: ByteArray::from_string("Some metadata").unwrap(),
+    let request = Request {
+        signer_id: context_public_key_felt,
+        nonce: 0,
+        kind: RequestKind::Context(
+            ContextRequest {
+                context_id: context_public_key_felt,
+                kind: ContextRequestKind::Add(
+                  alice_public_key_felt,
+                  Application {
+                      id: Felt::from_str("0x1234567890abcdef1234567890abcdef123456789").unwrap(),
+                      blob: Felt::from_str("0x1234567890abcdef1234567890abcdef123456789").unwrap(),
+                      size: 0,
+                      source: EncodableString("https://calimero.network".to_string()),
+                      metadata: EncodableString("Some metadata".to_string()),
+                  }
+              )
+            }
+        ),
+    };
+    
+    let mut serialized_request = vec![];
+    let _ = request.encode(&mut serialized_request).unwrap();
+    println!("serialized_request: {:?}", serialized_request);
+    // First hash (equivalent to poseidon_hash_span)
+    let first_hash = poseidon_hash_many(&serialized_request);
+    
+    // Second hash (equivalent to PoseidonTrait::new().update_with(...).finalize())
+    let hash = poseidon_hash_many(&[first_hash]);
+
+    let signature = context_key.sign(&hash).unwrap();
+
+    let signed_request = Signed {
+        payload: serialized_request,
+        signature_r: signature.r,
+        signature_s: signature.s,
     };
 
-    // let request = Request {
-    //     signer_id: alice_public_key_felt,
-    //     nonce: 0,
-    //     kind: RequestKind::Context(ContextRequest {
-    //         context_id: context_public_key_felt,
-    //         kind: ContextRequestKind::Add((context_public_key_felt, application.clone())),
-    //     }),
-    // };
-    // let mut serialized = vec![];
-    // // let _ = request.serialize(&mut serialized);
-    // let hash = poseidon_hash_many(&serialized);
-    // let signature = alice_key.sign(&hash).unwrap();
-    // let signed_request = Signed {
-    //     payload: serialized,
-    //     signature: (signature.r, signature.s),
-    // };
-    // // let mut signed_request_serialized = vec![];
-    // // let _ = signed_request
-    // //     .encode(&mut signed_request_serialized)
-    // //     .unwrap();
+    println!("signed_request: {:?}", signed_request);
 
-    // //Call the contract
-    // let provider = JsonRpcClient::new(HttpTransport::new(
-    //     Url::parse("https://starknet-sepolia.public.blastapi.io/rpc/v0_7").unwrap(),
-    // ));
+    let mut signed_request_serialized = vec![];
+    let _ = signed_request.encode(&mut signed_request_serialized).unwrap();
 
-    // let wallet_pk = LocalWallet::from(SigningKey::from_secret_scalar(
-    //     Felt::from_hex("0x3466a2196c72a94edd80c49baaad89c3cd71815038c31e1d3c94337ad97406d")
-    //         .unwrap(),
-    // ));
-    // let wallet_address =
-    //     Felt::from_hex("0x55d0c6f18991cebbf0dee233cafdd19f906f387a364b040c71c00094355e281")
-    //         .unwrap();
-    // let mut account = SingleOwnerAccount::new(
-    //     provider,
-    //     wallet_pk,
-    //     wallet_address,
-    //     chain_id::SEPOLIA,
-    //     ExecutionEncoding::New,
-    // );
+    println!("signed_request_serialized: {:?}", signed_request_serialized);
 
-    // let contract = ContextConfig::new(contract_id, account);
+    //Call the contract
+    let provider = JsonRpcClient::new(HttpTransport::new(
+        Url::parse("https://starknet-sepolia.public.blastapi.io/rpc/v0_7").unwrap(),
+    ));
 
-    // let response = contract.mutate(request).send().await.unwrap();
-    // println!("{:?}", response);
-    // println!("Account {:?}", account.address());
-    // account.set_block_id(BlockId::Tag(BlockTag::Pending));
+    let wallet_pk = LocalWallet::from(relayer_sk);
+    let wallet_address =
+        Felt::from_hex("0x55d0c6f18991cebbf0dee233cafdd19f906f387a364b040c71c00094355e281")
+            .unwrap();
+    let mut account = SingleOwnerAccount::new(
+        provider.clone(),
+        wallet_pk,
+        wallet_address,
+        chain_id::SEPOLIA,
+        ExecutionEncoding::New,
+    );
+    println!("signed_request_serialized: {:?}", signed_request_serialized);
+    account.set_block_id(BlockId::Tag(BlockTag::Pending));
 
-    // let result = account
-    //   .execute_v1(vec![Call {
-    //       to: Felt::from_str(contract_id).unwrap(),
-    //       selector: Felt::from_str("0x33f8af2c6d5b2376345a3c43ad230b0741fb5694c7064741c1927142bbd442a").unwrap(),
-    //       calldata: signed_request_serialized,
-    //   }])
-    //   .send()
-    //   .await
-    //   .unwrap();
-    
-    // println!("{:?}", result);
+    let result = account
+      .execute_v1(vec![Call {
+          to: Felt::from_str(contract_address).unwrap(),
+          selector: get_selector_from_name("mutate").unwrap(),
+          calldata: signed_request_serialized,
+      }])
+      .send()
+      .await
+      .unwrap();
 
+    println!("Result: {:?}", result);
+
+    let function_call = FunctionCall {
+        contract_address: Felt::from_str(contract_address).unwrap(),
+        entry_point_selector: get_selector_from_name("application").unwrap(),
+        calldata: vec![context_public_key_felt],
+    };
+
+    let result = provider.call(&function_call, BlockId::Tag(BlockTag::Latest)).await;
+    println!("{:?}", result);
 }
